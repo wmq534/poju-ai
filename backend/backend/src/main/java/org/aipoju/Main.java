e5378c2ce3ff506416ae67aa8f1bdc55eee572ba
wangmingqiang <wangmingqiang@kuaishou.com>
2025-04-05 22:25:14 +0800

    init project

---
+++ backend/backend/src/main/java/org/aipoju/Main.java
@@ -0,0 +1,12 @@
+package org.aipoju;
+
+/**
+ * @author xuzhihui
+ * @date 2025/4/5 22:19
+ * @description: ${description}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
